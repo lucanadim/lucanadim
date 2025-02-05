d33a5309448874a9fec28a26af5c85006143e471
robinbraemer <robin.braemer@web.de>
2025-02-05 21:15:17 +0100

    Initial sveltekit app

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg-[#181818]">
+    <div class="flex flex-nowrap">
+        
+    </div>
+</div>
