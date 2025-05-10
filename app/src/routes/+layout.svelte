cef202db8227de32175e6bdae2ec86d14099b85d
Luca Nadim <138592473+lucanadim@users.noreply.github.com>
2025-05-10 22:21:14 +0200

    Delete app directory

--- app/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import '../app.css';
-	let { children } = $props();
-</script>
-
-
-<div class="bg-[#181818] h-screen">
-	{@render children()}
-</div>
--- app/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:head>
-    <titel>Portfolio</titel>
-</svelte:head>
-    <div class="flex items-center h-screen">
-        <div class="bg-[#282828] rounded-2xl h-[95vh] ml-4 w-1/5">
-
-        </div>
-    </div>
